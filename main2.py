4ca6b8aa9d336ddf4faf8f8b423842612e7da623
yuinaTanaka <yuinatanaka@yuinaTanakanoMacBook-Air.local>
2025-08-10 22:41:23 +0900

    commit message

--- main2.py
+++ main2.py
@@ -5,50 +5,61 @@ class Player():
         self.name = name
     
     def dice_roll(self,game):
+        input(f"{self.name}の番です。")
         dice = random.randint(1,6)
         print(f"{self.name}の出た目：{dice}")
         game.countup()
         game.setPosition(self,dice)
 
+    #予期せぬデータが入ってきたときにエラーを防ぐためのもの
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.name == other.name
+        return False
+
 
 class Game():
     def __init__(self):
-        #self.p1 = Player("Yuki")
-        #self.p2 = Player("Yui")
 
         self.goal = 20
-        self.players = [{'player':Player("Yuki"),'position':0},{'player':Player("Yui"),'position':0}]
-        self.count = 0        
+        self.players = [
+            {'player':Player("Yuki"),'position':0},
+            {'player':Player("Yui"),'position':0},
+            {'player':Player("CPU"),'position':0},
+            {'player':Player("CPU2"),'position':0}
+            ]
+        self.count = 0  
+        self.player_len = len(self.players)
     
     def processing(self) -> bool:
-        return self.players[0].get('position') < self.goal and self.players[1].get('position') < self.goal #試合中
-        
+        for p in self.players:
+            if p.get('position') >= self.goal:
+                return False
+        return True 
 
-    def player(self) -> Player:
-        if self.count % 2 == 0:
-            return self.players[0].get('player')
-        else:
-            return self.players[1].get('player')
-
+    def current_player(self) -> Player:
+        num = self.count % self.player_len
+        return self.players[num]['player'] #num番目の要素の'player'を取り出す
+            
     def winner(self) -> Player:
         for p in self.players:
-            if(p.get('position') >= self.goal):
+            if p.get('position') >= self.goal:
                 return p.get('player')
 
     def countup(self):
-        self.count +=1
+         self.count +=1
 
     def setPosition(self,player,dice):
         for p in self.players:
             if(p.get('player') == player):
-                p['position'] = p.get('position') + dice
+                p['position'] += dice
                 
                 print(f"{p.get('player').name}のコマの状態：{p.get('position')}")      
 
 game = Game()
 
 while game.processing():
-    player = game.player()
+    player = game.current_player()
     player.dice_roll(game)
 
 print(f"{game.winner().name}の勝ち")
